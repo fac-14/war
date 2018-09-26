812ccc47cbaa17997784553983568353a9699fd3
Michael Watts <hello@michaelwatts.co>
2018-09-26 15:34:30 +0100

    make API call and get 2 piles of cards relates #11

---
+++ src/components/board.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import createDeck from '../../utils/getData';
+
+export default class Board extends React.Component {
+  state = {
+    player1: {
+      pile: [1, 2, 3, 4, 5],
+      playedPile: [],
+      score: 0,
+    },
+    player2: {
+      pile: [1, 2, 3, 4, 5],
+      playedPile: [],
+      score: 0,
+    },
+  };
+  componentDidMount = () => {
+    createDeck().then(data => console.log(data));
+  };
+  playGame = () => {
+    this.setState({ player1: { pile: [1, 2, 3] } });
+  };
+  render = () => {
+    return (
+      <main id="board">
+        <div>Player 1: {this.state.player1.pile.length} </div>
+        <div>Player 2: {this.state.player2.pile.length} </div>
+        <button id="play" onClick={this.playGame}>
+          Play!
+        </button>
+      </main>
+    );
+  };
+}
---
+++ utils/getData.js
@@ -0,0 +1,41 @@
+const checkResponse = response => {
+  if (response.status !== 200) {
+    return console.log(`Error with request ${response.status}`);
+  }
+  return response.json();
+};
+
+const getData = url => {
+  return fetch(url)
+    .then(checkResponse)
+    .catch(err => {
+      throw new Error('fetch getData failed ${err}');
+    });
+};
+
+const getDeckId = () => {
+  return getData(
+    'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+  ).then(data => data.deck_id);
+};
+
+const getCards = id => {
+  return getData(
+    `https://deckofcardsapi.com/api/deck/${id}/draw/?count=52`
+  ).then(response => response.cards);
+};
+
+const makeTwoPiles = cards => {
+  return {
+    pile1: cards.slice(0, 26),
+    pile2: cards.slice(26, 52),
+  };
+};
+
+const createDeck = () => {
+  return getDeckId()
+    .then(getCards)
+    .then(makeTwoPiles);
+};
+
+export default createDeck;
