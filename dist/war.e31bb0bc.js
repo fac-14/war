52b8afa2a92a44fc6bee75e958f9574c98a882b6
Simon Dupree <dupreenator@gmail.com>
2018-09-27 14:04:49 +0100

    player.js to functional component

--- dist/war.e31bb0bc.js
+++ dist/war.e31bb0bc.js
@@ -22192,73 +22192,24 @@ var _cardBack = _interopRequireDefault(require("../cardBack.png"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+var Player = function Player(_ref) {
+  var playerName = _ref.playerName,
+      score = _ref.score,
+      playedPile = _ref.playedPile;
+  var card = playedPile.slice(-1)[0];
+  return _react.default.createElement("div", null, _react.default.createElement("div", null, playerName, ": ", score), score ? _react.default.createElement("img", {
+    src: _cardBack.default
+  }) : _react.default.createElement("div", {
+    className: "emptySpace"
+  }), card ? _react.default.createElement("img", {
+    src: card.image
+  }) : _react.default.createElement("div", {
+    className: "emptySpace"
+  }));
+};
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-var Player =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(Player, _React$Component);
-
-  function Player() {
-    var _getPrototypeOf2;
-
-    var _this;
-
-    var _temp;
-
-    _classCallCheck(this, Player);
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    return _possibleConstructorReturn(_this, (_temp = _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Player)).call.apply(_getPrototypeOf2, [this].concat(args))), _this.state = {
-      name: _this.props.playerName,
-      score: _this.props.score,
-      card: _this.props.playedPile ? _this.props.playedPile.pop() : {}
-    }, _this.componentWillReceiveProps = function (nextProps) {
-      _this.setState({
-        score: nextProps.score,
-        card: nextProps.playedPile ? nextProps.playedPile[nextProps.playedPile.length - 1] : {}
-      });
-    }, _temp));
-  }
-
-  _createClass(Player, [{
-    key: "render",
-    value: function render() {
-      return _react.default.createElement("div", null, _react.default.createElement("div", null, this.state.name, ": ", this.state.score), this.state.score ? _react.default.createElement("img", {
-        src: _cardBack.default
-      }) : _react.default.createElement("div", {
-        className: "emptySpace"
-      }), this.state.card ? _react.default.createElement("img", {
-        src: this.state.card.image
-      }) : _react.default.createElement("div", {
-        className: "emptySpace"
-      }));
-    }
-  }]);
-
-  return Player;
-}(_react.default.Component);
-
-exports.default = Player;
+var _default = Player;
+exports.default = _default;
 },{"react":"node_modules/react/index.js","../cardBack.png":"src/cardBack.png"}],"node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
@@ -22477,7 +22428,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53826" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54841" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- src/components/player.js
+++ src/components/player.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import backCard from '../cardBack.png'
 
-export default class Player extends React.Component {
-    state = {
-        name: this.props.playerName,
-        score: this.props.score,
-        card: this.props.playedPile ? this.props.playedPile.pop() : {}
-    };
-    componentWillReceiveProps = nextProps => {
-        this.setState({
-            score: nextProps.score,
-            card: nextProps.playedPile ? nextProps.playedPile[nextProps.playedPile.length - 1] : {}
-        })
-    };
-    render() {
-        return (
-            <div>
-                <div>{this.state.name}: {this.state.score}</div>
-                {/* score bigger 0 ? then show back of card */}
-                {this.state.score ? <img src={backCard} /> : <div className="emptySpace" />}
-                {this.state.card ? <img src={this.state.card.image} /> : <div className="emptySpace" />}
-            </div>
-        )
-    }
+const Player = ({ playerName, score, playedPile }) => {
+    const card = playedPile.slice(-1)[0];
+    return (
+        <div>
+            <div>{playerName}: {score}</div>
+            {/* score bigger 0 ? then show back of card */}
+            {score ? <img src={backCard} /> : <div className="emptySpace" />}
+            {card ? <img src={card.image} /> : <div className="emptySpace" />}
+        </div>
+    );
 }
+
+export default Player;
